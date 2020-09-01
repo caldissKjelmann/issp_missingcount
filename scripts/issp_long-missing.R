b6036dd431ee5329228f5a03c636c518689f5a9c
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2020-09-01 11:13:43 +0200

    adding variable list

--- scripts/issp_long-missing.R
+++ scripts/issp_long-missing.R
@@ -40,13 +40,13 @@ issp_subset <- issp2017 %>%
   mutate(country = as_factor(country))
 
 #long mutation
-issp_long <- pivot_longer(issp_subset, cols = starts_with("v"), names_to = "question", values_to = "response") %>%
-  mutate(missing = as.numeric(case_when(question %in% vars_mis9899 & response == 99 ~ "1",
-                             question %in% vars_mis9899 & response == 98 ~ "1",
-                             question %in% vars_noans99 & response == 99 ~ "1",
-                             question %in% vars_noans9 & response == 9 ~ "1",
-                             question %in% vars_mis89 & response == 8 ~ "1",
-                             question %in% vars_mis89 & response == 9 ~ "1",
+issp_long <- pivot_longer(issp_subset, cols = starts_with("v"), names_to = "variable", values_to = "response") %>%
+  mutate(missing = as.numeric(case_when(variable %in% vars_mis9899 & response == 99 ~ "1",
+                             variable %in% vars_mis9899 & response == 98 ~ "1",
+                             variable %in% vars_noans99 & response == 99 ~ "1",
+                             variable %in% vars_noans9 & response == 9 ~ "1",
+                             variable %in% vars_mis89 & response == 8 ~ "1",
+                             variable %in% vars_mis89 & response == 9 ~ "1",
                              TRUE ~ "0")),
          year = 2017) %>%
   arrange(desc(missing))
@@ -60,6 +60,22 @@ issp_long <- issp_long %>%
          age = na_if(age, as.numeric(BIRTH) > 2017),
          SEX = as.factor(SEX)) %>%
   mutate(SEX = fct_drop(na_if(SEX, 9)))
+
+#labels
+library(labelled)
+library(purrr)
+issp2017_labels <- var_label(issp2017)
+issp2017_labelsdf <- data.frame(variable = names(issp2017_labels), label = sapply(issp2017_labels, "[[", 1), stringsAsFactors = FALSE)
+
+
+#export variable list
+issp_variables <- issp_long %>%
+  left_join(issp2017_labelsdf, by = "variable") %>%
+  group_by(variable, label) %>%
+  summarise(type = "")
+
+library(xlsx)
+write.xlsx(as.data.frame(issp_variables), "../output/issp2017_variables.xlsx", row.names = FALSE)
 
 # logit
 model <- glm(missing ~ SEX + age + country, family = binomial(link = "logit"), data = issp_long)
